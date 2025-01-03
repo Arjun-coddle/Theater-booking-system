89926e7f3a9b8678b021af69072626f511be6bf6
Arjun <arjun.k@coddletech.com>
2025-01-03 11:38:53 +0530

    change folder structure

--- server.js
+++ server.js
@@ -2,35 +2,20 @@ const express = require('express');
 const app = express();
 app.disable('x-powered-by');
 const path = require('path');
-const mysql = require('mysql2');
+const connection = require('./src/database/db');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12345',
-    database: 'assignment2'
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Database connection error:', err);
-        process.exit(1); 
-    }
-    console.log('Connected to the database!');
-});
-
-app.get('/', (req, res) => {   
+app.get('/', (req, res) => {
     connection.query('SELECT * FROM user', (err, result) => {
         if (err) {
             console.error('Database query error:', err);
             return res.status(500).json({ error: 'Database query failed' });
         }
-        res.json(result); 
+        res.json(result);
     });
 });
 
 app.get('/page1', (req, res) => {
-    res.sendFile(path.join(__dirname, "pages", "page1.html"), (err) => {
+    res.sendFile(path.join(__dirname, "src", "pages", "page1.html"), (err) => {
         if (err) {
             console.error('Error sending page1:', err);
             return res.status(500).send('Internal Server Error');
@@ -39,7 +24,7 @@ app.get('/page1', (req, res) => {
 });
 
 app.get('/page2', (req, res) => {
-    res.sendFile(path.join(__dirname, "pages", "page2.html"), (err) => {
+    res.sendFile(path.join(__dirname, "src", "pages", "page2.html"), (err) => {
         if (err) {
             console.error('Error sending page2:', err);
             return res.status(500).send('Internal Server Error');
